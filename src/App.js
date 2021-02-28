c206a6f411eba6d2279052f2ce15315a17c34607
Tomdasin <adad7tomi@yahoo.com>
2021-02-28 15:47:50 +0100

    first react_js app

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <div className="section-2 div">
+          <h2 className="section-2 h2">This is a React Js Project</h2>
+          <p>Hosting My First React Js App Website.</p>
+      </div>
     </div>
   );
 }
